46c6fd5a7e5da475096a1cd00b057b45a42d8a5b
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-05-26 13:47:33 -0300

    feat(categories): create route get all

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -12,4 +12,10 @@ categoriesRoutes.post("/", (req, res) => {
   return res.status(201).send();
 });
 
+categoriesRoutes.get("/", (req, res) => {
+  const all = categoryRepository.list();
+
+  return res.status(200).json({ all });
+});
+
 export { categoriesRoutes };
