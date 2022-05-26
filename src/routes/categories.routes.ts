83126fa11078a9f9b70d8cdbe618dab5b782e34b
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-05-26 12:55:58 -0300

    feat: add id using uuid

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { v4 as uuidV4 } from "uuid";
 
 const categoriesRoutes = Router();
 
@@ -7,7 +8,13 @@ const categories = [];
 categoriesRoutes.post("/categories", (req, res) => {
   const { name, description } = req.body;
 
-  categories.push({ name, description });
+  const category = {
+    name,
+    description,
+    id: uuidV4(),
+  };
+
+  categories.push(category);
 
   return res.status(201).send();
 });
