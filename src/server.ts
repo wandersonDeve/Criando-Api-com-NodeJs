1bfd4a6373ab73aa80e1953ff56070fe59ecdb5a
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-05-26 12:03:51 -0300

    test(debug): settings for debug tests

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,17 @@ const app = express();
 
 const port = 8000;
 
-app.get('/', (req, res) => {
-  return res.json({message: 'Server online'})
-})
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.json({ message: "Server online" });
+});
+
+app.post("/courses", (req, res) => {
+  const { name } = req.body;
+
+  return res.json({ name });
+});
 
 app.listen(port, () => {
   console.info(`Server is running at http://localhost:${port}`);
