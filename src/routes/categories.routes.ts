38b7465f2691a1e694226e4634fad6822dd19989
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-05-26 12:41:36 -0300

    feat(categories): create route post in categories

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/categories", (req, res) => {
+  const { name, description } = req.body;
+
+  categories.push({ name, description });
+
+  return res.status(201).send();
+});
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,12 @@
 import express from "express";
-
+import { categoriesRoutes } from "./routes/categories.routes";
 const app = express();
 
 const port = 8000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Server online" });
-});
-
-app.post("/courses", (req, res) => {
-  const { name } = req.body;
-
-  return res.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(port, () => {
   console.info(`Server is running at http://localhost:${port}`);
