e6fa8cbec5ba14db22ca5f7f13c5eb3ec272ca86
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-05-26 11:41:11 -0300

    style(tsconfig): disable strict mode

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,10 @@ const app = express();
 
 const port = 8000;
 
+app.get('/', (req, res) => {
+  return res.json({message: 'Server online'})
+})
+
 app.listen(port, () => {
   console.info(`Server is running at http://localhost:${port}`);
 });
