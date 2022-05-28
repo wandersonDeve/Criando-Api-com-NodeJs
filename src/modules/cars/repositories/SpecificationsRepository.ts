6329b509635154772f2f0147ba1f677b4d062ea6
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-05-28 14:39:27 -0300

    feat(specifications): create specifications repository

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,35 @@
+import { Specification } from "../model/Specifications";
+import {
+  ISpecificationsDTO,
+  ISpecificationsRepository,
+} from "./ISpecificationsRepository";
+
+class SpecificationsRepository implements ISpecificationsRepository {
+  private specifications: Specification[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ISpecificationsDTO): void {
+    const specification = new Specification();
+
+    Object.assign(specification, {
+      name,
+      description,
+      createAt: new Date(),
+    });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): Specification {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+
+    return specification;
+  }
+}
+
+export { SpecificationsRepository };
