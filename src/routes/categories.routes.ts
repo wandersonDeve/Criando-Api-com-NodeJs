1dd091e2e5316f5df0cfff77566c8d4e509cdcce
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-05-28 15:00:53 -0300

    refactor(category): create category controller

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,14 @@
 import { Router } from "express";
 
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
 import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRepository = new CategoriesRepository();
 
 const categoriesRoutes = Router();
 
 categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return res.status(201).send();
+  return createCategoryController.handle(req, res);
 });
 
 categoriesRoutes.get("/", (req, res) => {
