6ac55391d397f527bae029f0a8df0235161ddf5e
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-05-28 02:42:50 -0300

    feat: create ICategoriesRepositories

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { CreateCategoryService } from "../services/CreateCategoryService";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
+import { CreateCategoryService } from "../services/CreateCategoryService";
+import { PostgresCategoriesRepository } from "../repositories/PostgresCategoriesRepository";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = new PostgresCategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
