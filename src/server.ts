fde2dff186b2a00048eddd598a9a35cbd0d3743e
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-06-01 11:52:41 -0300

    feat: create documentation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
@@ -8,7 +10,9 @@ const port = 8000;
 
 app.use(express.json());
 
-app.use(router)
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(router);
 
 app.listen(port, () => {
   console.info(`Server is running at http://localhost:${port}`);
