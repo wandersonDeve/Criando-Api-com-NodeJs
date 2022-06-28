871cac2a78936f17da2606fe1fbd7d06b3ad335e
Wanderson <wanderson.santos@gcbinvestimentos.com>
2022-06-28 09:16:12 -0300

    feat: create migration car image

---
+++ src/shared/infra/typeorm/migrations/1656418093665-CreateCarImages.ts
@@ -0,0 +1,44 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateCarImages1656418093665 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "car_images",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+          },
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "image_name",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKCarImage",
+            referencedTableName: "cars",
+            referencedColumnNames: ["id"],
+            columnNames: ["car_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("car_images");
+  }
+}
